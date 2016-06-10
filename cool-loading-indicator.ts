8b93c171f32b4411307a0608eb1c40cc9e266046
Barna Toth <Barna.Toth@logmein.com>
2016-06-10 10:05:37 +0200

    Use new cool-http

--- cool-loading-indicator.ts
+++ cool-loading-indicator.ts
@@ -49,7 +49,7 @@ export class CoolLoadingIndicator implements OnInit, OnDestroy, IRequestIntercep
             this.showIndicator = true;
         }
 
-        return Promise.resolve();
+        return Promise.resolve(false);
     }
 
     afterResponseAsync() {
@@ -59,7 +59,7 @@ export class CoolLoadingIndicator implements OnInit, OnDestroy, IRequestIntercep
             this.showIndicator = false;
         }
 
-        return Promise.resolve();
+        return Promise.resolve(false);
     }
 
     get shouldShowIndicator() {
